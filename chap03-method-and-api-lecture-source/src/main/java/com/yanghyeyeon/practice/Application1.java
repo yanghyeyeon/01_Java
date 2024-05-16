ccf1f5b3e575d3e43462ed37a87a34b853be39d8
yanghyeyeon <yhy4058@gmail.com>
2024-05-16 18:09:29 +0900

    메소드 연습

--- chap03-method-and-api-lecture-source/src/main/java/com/yanghyeyeon/practice/Application1.java
+++ chap03-method-and-api-lecture-source/src/main/java/com/yanghyeyeon/practice/Application1.java
@@ -1,4 +1,35 @@
 package com.yanghyeyeon.practice;
 
 public class Application1 {
+    public static void main(String[] args) {
+
+        int first = 10;
+        int second =20;
+
+        Application1 app1 = new Application1();
+
+        System.out.println("최소값 : " + app1.minNumberOf(first,second));
+
+        System.out.println("두 수의 합 : " + Application1.plusTwoNumber(first,second));
+
+
+        Calculator calc = new Calculator();
+
+        int plus = calc.plusTwoNumber(first,second);
+        System.out.println("두 수의 합 : " + plus);
+
+        int max = Calculator.maxNumber(first,second);
+        System.out.println("최댓값 : " + max);
+
+
+    }
+    public int minNumberOf(int first, int second) {
+
+        return (first > second) ? second : first;
+    }
+
+    public static int plusTwoNumber(int first, int second){
+
+        return first + second;
+    }
 }
---
+++ chap03-method-and-api-lecture-source/src/main/java/com/yanghyeyeon/practice/Calculator.java
@@ -0,0 +1,13 @@
+package com.yanghyeyeon.practice;
+
+public class Calculator {
+    public int plusTwoNumber(int first, int second){
+
+        return first + second;
+    }
+
+    public static int maxNumber(int first, int second){
+
+        return (first > second) ? first : second;
+    }
+}
