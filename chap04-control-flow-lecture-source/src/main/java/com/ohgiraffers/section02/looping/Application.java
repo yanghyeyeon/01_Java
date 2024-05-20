d0ea58bbbed4c79fcbba47b488306f3ec39c4828
yanghyeyeon <yhy4058@gmail.com>
2024-05-20 14:22:15 +0900

    looping

--- chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/Application.java
+++ chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/Application.java
@@ -1,2 +1,24 @@
-package com.ohgiraffers.section02.looping;public class Application {
+package com.ohgiraffers.section02.looping;
+
+public class Application {
+    public static void main(String[] args) {
+
+        A_for a_for = new A_for();
+
+        //a_for.testSimpleForStatement();
+        //a_for.testForExample1();
+        //a_for.testForExample2();
+        //a_for.testForExample3();
+        //a_for.printSimpleGugudan();
+        //a_for.testForExample4();
+        //a_for.testExample0();
+
+        A_nestedFor a_nested_for = new A_nestedFor();
+        //a_nested_for.printGugudanFromTwoToNine();
+        //a_nested_for.printUpgradeGugudanFromTwoToNine();
+        //a_nested_for.printStarInputRowTimes();
+        a_nested_for.printTriangleStars();
+
+
+    }
 }
