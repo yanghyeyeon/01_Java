b4fe983ea9b763f6224c720f7d6bc266fd06f5e7
yanghyeyeon <yhy4058@gmail.com>
2024-05-21 13:17:50 +0900

    2차원 배열

--- chap05-array-lecture-source/src/main/java/com/ohgiraffers/section02/demensinal/Application1.java
+++ chap05-array-lecture-source/src/main/java/com/ohgiraffers/section02/demensinal/Application1.java
@@ -1,2 +1,61 @@
-package com.ohgiraffers.section02.demensinal;public class Application1 {
+package com.ohgiraffers.section02.demensinal;
+
+public class Application1 {
+    public static void main(String[] args) {
+        /*
+        * 다차원 배열
+        *
+        * 다차원 배열은 2차원 이상의 배열을 의미한다.
+        * 배열의 인덱스마다 또 다른 배열의 주소를 보관한느 배열을 의미한다.
+        * -> 2차원 배열은 1차원 배열 여러개를 하나로 묶어서 관리하는 배열을 의미한다.
+        * */
+
+        /*
+        * 2차원 배열을 사용하는 방법
+        * 1. 배열의 주소를 보관하는 레퍼런스 변수 선어(stack)
+        * 2. 여러개의 1차원 배열의 주소를 관리하는 배열을 생성(heap)
+        * 3. 각 인덱스에서 관리하는 배열을 할당하여 주소를 보관하는 배열에 저장
+        * 4. 생성한 여러개의 1차원 배열에 차례로 접근해서 사용
+        * */
+
+        int[][] arr;
+        int arr2[][];
+        // int[] arr[];
+
+        //arr = new int[][]; // 크기를 지정하지 않으면 에러 발생
+        //arr2 = new int [][2];
+        arr = new int[3][]; // 주소를 묶어서 관리할 배열의 크기를 지정해야함.
+
+        arr[0] = new int [5];
+        arr[1] = new int [5];
+        arr[2] = new int [6];
+
+        arr2 = new int [3][5];
+
+        // 크기 할당 후에, 아무 값을 대입하지 않아도,
+        // heap 영역에는 값이 없는 상태로 공간 생성이 불가능 하기 때문에, 기본값이 들어있다.
+
+        //arr2[0]에 있는 배열 값을 출력
+        for(int i = 0; i < arr2[0].length; i++) {
+            System.out.print(arr2[0][i] + " ");
+        }
+        System.out.println();
+        for(int i = 0; i < arr2[1].length; i++) {
+            System.out.print(arr2[1][i] + " ");
+        }
+        System.out.println();
+        for(int i = 0; i < arr2[2].length; i++) {
+            System.out.print(arr2[2][i] + " ");
+        }
+        System.out.println();
+
+        System.out.println("======= 이중 for 문으로 이차원배열 출력 =======");
+        for (int j = 0; j <arr2.length; j++){
+            for(int i = 0; i < arr2[j].length; i++) {
+                System.out.print(arr2[j][i] + " ");
+            }
+            System.out.println();
+        }
+
+    }
 }
---
+++ chap05-array-lecture-source/src/main/java/com/ohgiraffers/section02/demensinal/Application2.java
@@ -0,0 +1,55 @@
+package com.ohgiraffers.section02.demensinal;
+
+public class Application2 {
+    public static void main(String[] args) {
+
+        /*
+        * 이차원 배열의 선언
+        *
+        * 자료형[][] 변수명 = new 자료형[할당할 배열의 갯수][할당할 배열의 길이];
+        * */
+
+        int[][] iarr = new int[3][6];
+
+        iarr[0][0] = 1;
+        iarr[0][1] = 2;
+        iarr[0][2] = 3;
+        iarr[0][3] = 4;
+        iarr[0][4] = 5;
+
+        iarr[1][0] = 6;
+        iarr[1][1] = 7;
+        iarr[1][2] = 8;
+        iarr[1][3] = 9;
+        iarr[1][4] = 10;
+
+        iarr[2][0] = 11;
+        iarr[2][1] = 12;
+        iarr[2][2] = 13;
+        iarr[2][3] = 14;
+        iarr[2][4] = 15;
+
+        System.out.println("0번째 인덱스 배열");
+        System.out.print(iarr[0][0] + " ");
+        System.out.print(iarr[0][1] + " ");
+        System.out.print(iarr[0][2] + " ");
+        System.out.print(iarr[0][3] + " ");
+        System.out.print(iarr[0][4] + " ");
+        System.out.println();
+
+        System.out.println("1번째 인덱스 배열");
+        System.out.print(iarr[1][0] + " ");
+        System.out.print(iarr[1][1] + " ");
+        System.out.print(iarr[1][2] + " ");
+        System.out.print(iarr[1][3] + " ");
+        System.out.print(iarr[1][4] + " ");
+        System.out.println();
+
+        System.out.println("2번째 인덱스 배열");
+        System.out.print(iarr[2][0] + " ");
+        System.out.print(iarr[2][1] + " ");
+        System.out.print(iarr[2][2] + " ");
+        System.out.print(iarr[2][3] + " ");
+        System.out.print(iarr[2][4] + " ");
+    }
+}
