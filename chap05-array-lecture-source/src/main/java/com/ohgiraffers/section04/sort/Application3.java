e0c4fa0d3a08ec0bccaf0df840490d501ffb25f2
yanghyeyeon <yhy4058@gmail.com>
2024-05-23 10:19:46 +0900

    배열 선택정렬

--- chap05-array-lecture-source/src/main/java/com/ohgiraffers/section04/sort/Application3.java
+++ chap05-array-lecture-source/src/main/java/com/ohgiraffers/section04/sort/Application3.java
@@ -1,2 +1,21 @@
-package com.ohgiraffers.section04.sort;public class Application3 {
+package com.ohgiraffers.section04.sort;
+
+public class Application3 {
+
+    // 선택 정렬
+    public static void main(String[] args) {
+        /*
+        * 배열을 전부 탐색하여 최소값을 고르고 왼쪽부터 채워나가는 방식의 정렬
+        * 데이터의 양이 적을 때 좋은 성능 나타낸다(교환회수가 적음)
+        * 하지만 배열을 전부 탐색하여 최소값을 찾아야 하기 때문에
+        * 100개 이상의 자료에서는 급격하게 속도가 저하된다.
+        * */
+
+        int[] arr = {2, 5, 4, 6, 1, 3};
+
+        int min; // 최소값을 가지니 데이터의 인데스 저장
+        int temp;
+
+
+    }
 }
