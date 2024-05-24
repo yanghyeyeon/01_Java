1032ea376127032ca4b5819a6c2dc9cf0bed1835
yanghyeyeon <yhy4058@gmail.com>
2024-05-24 12:21:24 +0900

    dto

--- chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section04/dto/Application.java
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section04/dto/Application.java
@@ -1,2 +1,42 @@
-package com.ohgiraffers.section04.dto;public class Application {
+package com.ohgiraffers.section04.dto;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        // 생성자를 이용한 초기화 vs 설정자를 이용한 초기화
+
+        /*
+        * 1. 생성자를 이용한 초기화
+        * 장점 : setter메서드를 여러번 호출해서 사용하지 않고, 단 한번의 호출로 인스턴스 생성 및 초기화 가능
+        * 단점 : 필드를 초기화할 매개변수의 갯수를 경우의 수 별로 모두 만들어줘야한다.
+        *       호출시 인자가 많아지는 경우 어떠한 값이 어떤 필드를 의미하는지 한눈에 보기 힘들다.
+        *       예시) 아이디, 비밀번호, 이름이 모두 ohgiraffers 인경우
+        *       new User("ohgiraffers", "ohgiraffers", "ohgiraffers");
+        *
+        * 2. 설정자를 이용한 초기화
+        * 장점 : 필드를 초기화 하는 각각의 값들이 어떤 필드를 초기화 하는지 명확히 볼 수 있다.
+        *       예) 아이디, 비밀번호, 이름이 모두 ohgiraffers 인 경우
+        *       User user = new USer();
+        *       user.setId("ohgiraffers");
+        *       user.setPwd("ohgiraffers");
+        *       user.setName("ohgiraffers");
+        *       호출한 코드만 봐도 어떤 값이 어떤 필드를 초기화하는 내용인지 쉽게 알 수 있다.
+        * 단점 : 하나의 인스턴스를 생성할때, 하나의 호출로 끝나지 않는다.
+        * */
+
+        // 1. 생성자를 이용한 초기화
+        UserDTO user1 = new UserDTO("ohgiraffers","ohgiraffers","ohgiraffers",
+                new java.util.Date());
+
+
+        // 기본 생성자를 이용한 초기화 + 설정자(Setter)
+        UserDTO user2 = new UserDTO();
+        user2.setId("ohgiraffers");
+        user2.setPwd("ohgiraffers");
+        user2.setName("ohgiraffers");
+        user2.setEnrollDate(new java.util.Date());
+
+        System.out.println(user2.getInfo());
+    }
 }
--- chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section04/dto/UserDTO.java
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section04/dto/UserDTO.java
@@ -1,2 +1,85 @@
-package com.ohgiraffers.section04.dto;public class UserDTO {
+package com.ohgiraffers.section04.dto;
+
+import java.util.Date;
+
+public class UserDTO {
+
+    /*
+    * 자바빈(Java Bean)
+    * JSP에서 배우게 될 표준 액션 태그로 접근할 수 있는 자바 클래스이다.
+    * 자바 코드를 모르는 웹 퍼블리셔들도 자바 코드를 사용 할 수 있도록 태그 형식으로 지원하는 문법
+    * 이 때, 사용할 수 있도록 규칙을 정해놓은 java 클래스를 자바빈(java bean)이라고 부른다.
+    *
+    * 지금은. 특정 목적에 따라 클래스를 작성하는 규칙이라고 보면 됨.
+    * */
+
+    /*
+    * 자바빈 작성 규칙
+    * 1. 자바빈은 특정 패키지에 속해있어야 함. (default 피키지 사용금지)
+    * 2. 멤버변수의 접근제어자는 private으로 선언해야함.
+    * 3. 기본 생성자가 명시적으로 존재해야 한다. (매개변수는 선택사항)
+    * 4. 모든 멤버 변수에 접근 가능한 설정자(setter)와 접근자(getter)가 public으로 작성되어 있어야함
+    * 5. 직렬화가 되어있어야 한다. (선택사항)
+    * */
+
+    // 멤버 변수의 접근제어자는 private으로 선언해야함.
+    private String id;
+    private String pwd;
+    private String name;
+    private java.util.Date enrollDate;
+
+    // 기본생성자를 명시적으로 작성
+    public UserDTO() {}
+
+
+    // 매개변수 있는 생성자는 선택 사항
+    // 매개변수 있는 생성자 중 가장 많이 사용되는것은 모든 필드 초기화 하는 생성자이다.
+    public UserDTO(String id, String pwd, String name, Date enrollDate) {
+        this.id = id;
+        this.pwd = pwd;
+        this.name = name;
+        this.enrollDate = enrollDate;
+    }
+
+    /*
+    * 설정자(setter)와 접근자(getter) -> 모든 필드에 대해 만들어야하고 public으로 만들어야함.
+    * */
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getEnrollDate() {
+        return enrollDate;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEnrollDate(Date enrollDate) {
+        this.enrollDate = enrollDate;
+    }
+
+
+    public String getInfo() {
+        return "UserDTO [id" + this.id + ", pwd=" + this.pwd + ", name=" + this.name +
+                ", enrolllDate=" + this.enrollDate + "]";
+    }
 }
