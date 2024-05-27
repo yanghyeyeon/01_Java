d90a630079883b50ed47080ca2cb51930c42b3dd
yanghyeyeon <yhy4058@gmail.com>
2024-05-27 14:21:43 +0900

    lazy singleton

--- chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section06/singleton/LazySingleton.java
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section06/singleton/LazySingleton.java
@@ -1,2 +1,23 @@
-package com.ohgiraffers.section06.singleton;public class LazySingleton {
+package com.ohgiraffers.section06.singleton;
+
+public class LazySingleton {
+
+    // 클래스가 초기화 되는 시점에 정적 필드를 선언해두고 null 초기화가 됨
+    private static LazySingleton lazy;
+
+    // 생성자 호출을 통한 인스턴스 생성 제한
+    private LazySingleton() {}
+
+    public  static LazySingleton getInstance() {
+
+        /*
+        * 인스턴스를 생성한 적이 없는 경우 인스턴스를 생성해서 반환하고
+        * 생성한 인스턴스가 있는 경우 만들어둔 인스터스를 반환한다.
+        * */
+        if(lazy == null) {
+            lazy = new LazySingleton();
+        }
+
+        return lazy;
+    }
 }
