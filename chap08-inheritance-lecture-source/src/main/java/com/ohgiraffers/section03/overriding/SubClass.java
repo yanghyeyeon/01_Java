297b450eb7116ce0c385353a488706058e681ad9
yanghyeyeon <yhy4058@gmail.com>
2024-05-28 12:39:23 +0900

    상속 overriding

--- chap08-inheritance-lecture-source/src/main/java/com/ohgiraffers/section03/overriding/SubClass.java
+++ chap08-inheritance-lecture-source/src/main/java/com/ohgiraffers/section03/overriding/SubClass.java
@@ -17,5 +17,28 @@ public class SubClass extends SuperClass {
 //    @Override
 //    public void method(int num, int num2){}
 
-    //
+    // 4. 메소드 이름, 리턴타입, 매개변수 갯수, 매개변수 타입
+    @Override
+    public void method(int num) {}
+
+    // 5. private 메소드는 오버라이딩 불가
+//    @Override
+//    private void privateMethod(){}
+
+    // 6. final 메소드 오버라이딩 불가 -> 변경이 불가하기 때문
+//    @Override
+//    public final void finalMethod() {}
+
+    // 7. 부모 메소드의 접근제한자 같거나 더 젋은 버위로 오버라이딩 가능
+//        @Override
+//        void protectedMethod() {} // 더 좁은 범위로는 불가능
+
+//        @Override
+//        protected  void protectedMethod() []
+
+    @Override
+    public void protectedMethod() {} // 더 넓은 범위로 가능
+
+    // 클래스에서도 final 키워드를 추가 할 수 있는데.
+    // 이는 상속을 제한하는 키워드이다.
 }
