ca9a18646f3430b8135c692bad1f9c4c4c799cf8
yanghyeyeon <yhy4058@gmail.com>
2024-05-28 11:23:45 +0900

    coffee project

--- chap06-class-and-object-lecture-source/src/main/java/com/yanghyeyeon/subproject/Barista.java
+++ chap06-class-and-object-lecture-source/src/main/java/com/yanghyeyeon/subproject/Barista.java
@@ -2,22 +2,33 @@ package com.yanghyeyeon.subproject;
 
 public class Barista {
     /*
-    * 주문에 따라 커피를 만들어야함.
-    * */
+     * 주문에 따라 커피를 만들어야함.
+     * */
 
-    private int coffeeStock = 5000; // 5000g 커피보유
+    private static int coffeeStock = 200; // 5000g 커피보유
 
     public Coffee[] makeCoffee(Order order) {
 
         // 재고 차감 처리 - 커피한잔에 20g 사용
+        if (coffeeStock >= order.getCount() * 20) {
 
-        // 커피 생성
-        Coffee[] coffees = new Coffee[order.getCount()];
+            // 커피 생성
+            Coffee[] coffees = new Coffee[order.getCount()];
 
-        for (int i = 0; i < coffees.length; i++) {
-            coffees[i] = new Coffee(order.getMenuNAme(), order.getOption());
+            // 보유량 차감
+            coffeeStock = coffeeStock - order.getCount()*20;
+
+            for (int i = 0; i < coffees.length; i++) {
+                coffees[i] = new Coffee(order.getMenuNAme(), order.getOption());
+            }
+
+            return coffees;
+
+        }else {
+            System.out.println("[바리스타] : 커피가루 충전해주세요!!!!");
+            return null;
         }
 
-        return coffees;
+
     }
 }
--- chap06-class-and-object-lecture-source/src/main/java/com/yanghyeyeon/subproject/Kiosk.java
+++ chap06-class-and-object-lecture-source/src/main/java/com/yanghyeyeon/subproject/Kiosk.java
@@ -59,7 +59,13 @@ public class Kiosk {
 
         // Arrays : 배열에 관련된 기능을 미리 만들어놓은 패키지
         // toString : 객체 안에 있는 값을 꺼내서 보여줌.
-        System.out.println("> 주문하신 커피" + Arrays.toString(coffees));
+        if ( coffees != null) {
+
+            System.out.println("> 주문하신 커피" + Arrays.toString(coffees));
+        }else {
+            System.out.println("주방에 커피가루가 부족합니다. 죄송합니다.");
+        }
+
     }
 
     private Order createOrder(String menuName) {
