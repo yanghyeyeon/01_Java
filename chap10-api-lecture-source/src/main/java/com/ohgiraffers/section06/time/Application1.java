7752b055cabda74c02dd09a9f013a8289d4a7c09
yanghyeyeon <yhy4058@gmail.com>
2024-05-29 15:11:31 +0900

    api time

--- chap10-api-lecture-source/src/main/java/com/ohgiraffers/section06/time/Application1.java
+++ chap10-api-lecture-source/src/main/java/com/ohgiraffers/section06/time/Application1.java
@@ -1,2 +1,46 @@
-package com.ohgiraffers.section06.time;public class Application1 {
+package com.ohgiraffers.section06.time;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Application1 {
+
+    // time 패키지 => Date, Calendar가 가지고 있는 단점을 해소하기위해 탄생
+    public static void main(String[] args) {
+        /*
+        * time 패키지에는
+        * LocalTime, LocalDate, LocalDateTime, ZoneDateTime
+        * 클래스들이 있다.
+        * */
+
+        /*
+        * LocalTime
+        * 시간을 표현하는 클래스
+        * - 시, 분, 초, 나노초
+        * - 날짜 정보를 포함하지 않는다.
+        * - 14:30:15.123
+        * */
+
+        LocalTime timeNow = LocalTime.now();
+        System.out.println("timeNow = " + timeNow);
+        LocalTime timeOf = LocalTime.of(15,25,1);
+        System.out.println("timeOf = " + timeOf);
+
+        /*
+        * LocalDate
+        * 날짜를 표현하는 클래스
+        * - 연도, 월, 일
+        * - 시간 정보를 포함하지 않는다.
+        * - 2024-05-29
+        * */
+        LocalDate dateNow = LocalDate.now();
+        System.out.println("dateNow = " + dateNow);
+        LocalDate dateOf = LocalDate.of(2024,5,29);
+        System.out.println("dateOf + " + dateOf);
+
+        /*
+        * ZoneDateTime
+        * 날짜와 시간, 시간대를 함께 포함하는 클래스
+        * */
+    }
 }
