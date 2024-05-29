b061e62e2ee541772f8f83f670e562210b19a588
yanghyeyeon <yhy4058@gmail.com>
2024-05-29 22:32:25 +0900

    api 연습문제

--- chap10-api-lecture-source/src/main/java/com/ohgiraffers/api/split/SplitTest.java
+++ chap10-api-lecture-source/src/main/java/com/ohgiraffers/api/split/SplitTest.java
@@ -5,14 +5,16 @@ public class SplitTest {
     public static void main(String[] args) {
 
         String char1 = "J a v a P r o g r a m";
-        String[] chArry = char1.split(" ");
+        String[] sArry = char1.split(" ");
 
-        for(int i = 0; i < char1.length(); i++) {
-            System.out.println("chArry [" + i + "] = " + chArry[i]);
+        for(int i = 0; i < sArry.length; i++) {
+            System.out.println("chArry [" + i + "] = " + sArry[i]);
         }
 
         System.out.println("분리전 글자 = " + char1 + ", 글자 갯수 = " + char1.length());
 
-        System.out.println("분리 후 문자배열 갯수 = " + chArry.length);
+        System.out.println("분리 후 문자배열 갯수 = " + sArry.length);
+        
+
     }
 }
