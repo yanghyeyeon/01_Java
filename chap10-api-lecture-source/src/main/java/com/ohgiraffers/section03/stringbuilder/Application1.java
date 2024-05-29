a3c8baaa720155b56eaeeafd1e12a79b70e7ff63
yanghyeyeon <yhy4058@gmail.com>
2024-05-29 13:18:17 +0900

    api stringbuilder

--- chap10-api-lecture-source/src/main/java/com/ohgiraffers/section03/stringbuilder/Application1.java
+++ chap10-api-lecture-source/src/main/java/com/ohgiraffers/section03/stringbuilder/Application1.java
@@ -1,2 +1,50 @@
-package com.ohgiraffers.section03.stringbuilder;public class Application {
+package com.ohgiraffers.section03.stringbuilder;
+
+public class Application1 {
+
+    // String과 Stringbulider의 차이점
+    public static void main(String[] args) {
+
+        /*
+        * String과 StringBuilder
+        * String : 불변이라는 특성을 가지고 있다.
+        *          문자열에 + 연산으로 합치기 하는 경우, 기존 인스턴스를 수정하는 것이 아닌,
+        *          새로운 인스턴스를 반환한다.
+        *          문자열 변경이 자주 일어나는 경우 성능 면에서 좋지 않다.
+        *          하지만, 변하지 않는 문자열 자주 읽어 들이는 경우 오히려 더 좋은 성능을 낸다.
+        * StringBuilder : 가변이라는 특징을 갖는다.
+        *                 문자열에 append() 메소드를 이용해 합치는 경우
+        *                 기존 인스턴스를 수정하기 때문에 새로운 인스턴스를 만들지 않는다.
+        *                 따라서 잦은 문자열 변경이 일어나는 경우 String보다 성능이 좋다.
+        * */
+
+        /*
+        * StringBuilder : 스레드 동기화 기능 제공되지 않음
+        *                 스레드 동기화 처리를 고려하지 ㅏㅇㄶ는 상황에서 StringBuffer보다 성능이 좋음
+        *
+        * StringBuffer : 스레드 동기화 기능 제공한다.
+        *                성능 면에서 StringBuilder보다는 느리다.
+        * */
+
+        // StringBuilder 인스턴스 생성
+        StringBuilder sb = new StringBuilder("java");
+
+        System.out.println("sb = " + sb);
+
+        /*
+        * hashCode는 오버라이딩 안되어 있음.
+        * => 동일한 값을 가지는 경우 같은 해쉬코드를 반환하는 ㅓㄳ이 아니라.
+        *    인스턴스가 동일해야 같은 해쉬코드를 반환한다.
+        * */
+        System.out.println("sb hashCode : " + sb.hashCode());
+
+        sb.append("oracle");
+
+        System.out.println("sb = " + sb);
+
+        System.out.println("sb hashCode : " + sb.hashCode());
+
+        // StringBuilder는 문자열을 변경했다고 새로운 인스턴스가 생성되지 않는다.
+
+    }
 }
--- chap10-api-lecture-source/src/main/java/com/ohgiraffers/section03/stringbuilder/Application2.java
+++ chap10-api-lecture-source/src/main/java/com/ohgiraffers/section03/stringbuilder/Application2.java
@@ -1,2 +1,30 @@
-package com.ohgiraffers.section03.stringbuilder;public class Application2 {
+package com.ohgiraffers.section03.stringbuilder;
+
+public class Application2 {
+
+    // StringBuilder의 자주 사용되는 메서드
+    public static void main(String[] args) {
+
+        /*
+        * delete() : 시작 인덱스와 종료 인덱스를 이용해서 문자열에서 원하는 부분의 문자열 제거
+        * deleteCharAt() : 문자열 인덱스를 이용해서 문자 하나를 제거
+        * => 둘다 원본에 영향을 미친다.
+        * */
+
+        StringBuilder sb = new StringBuilder("javaoracle");
+        StringBuilder sb2 = new StringBuilder("javaoracle");
+
+        System.out.println("sb = " + sb);
+
+        System.out.println("delte() : " + sb.delete(2, 5));
+
+        System.out.println("sb = " + sb);
+
+        System.out.println("sb2 = " + sb2);
+
+        System.out.println("delteCharAt() : " + sb2.deleteCharAt(2));
+
+        System.out.println("sb2 = " + sb2);
+
+    }
 }
