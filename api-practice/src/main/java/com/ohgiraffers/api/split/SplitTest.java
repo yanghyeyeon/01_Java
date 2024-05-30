a7441bb043fe5fef349a6c255eedf34ec112d000
yanghyeyeon <yhy4058@gmail.com>
2024-05-30 11:11:52 +0900

    api split 연습

--- api-practice/src/main/java/com/ohgiraffers/api/split/SplitTest.java
+++ api-practice/src/main/java/com/ohgiraffers/api/split/SplitTest.java
@@ -15,9 +15,27 @@ public class SplitTest {
         // 1) 문자배열을 문자열배열 크기로 만들기
         char[] chArr = new char[strArr.length];
 
+        // 2) 문자열배열의 값을 문자로 바꿔서, 문자배열에 값넣기
         for (int i = 0; i < strArr.length; i++) {
-                
+            chArr[i] = strArr[i].charAt(0);
         }
+        System.out.println("문자배열 갯수 출력 : " + chArr.length);
+        // 문자배열에 값이 잘들어갔는지 확인
+        System.out.println("문자배열 내용 출력 : " + Arrays.toString(chArr));
+
+        // 문자배열 -> 문자열
+        String result = "";
+        for(char ch : chArr) {
+            result = result + ch;
+        }
+        System.out.println("result = " + result);
+        
+        String result2 = new String(chArr);
+        System.out.println("result2 = " + result2);
+
+        // 대문자로 바꾸기
+        result = result.toUpperCase();
+        System.out.println("대문자로 바꾼 result = " + result);
 
 //        for (String s : strArr) {
 //            System.out.println(s);
