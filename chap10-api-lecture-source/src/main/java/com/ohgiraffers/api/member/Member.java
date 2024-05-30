20a02c2e1f62830ffefeb676d9e822346c522987
yanghyeyeon <yhy4058@gmail.com>
2024-05-30 18:10:45 +0900

    csv 연습문제

---
+++ chap10-api-lecture-source/src/main/java/com/ohgiraffers/api/member/Member.java
@@ -0,0 +1,74 @@
+package com.ohgiraffers.api.member;
+
+import java.time.LocalDate;
+
+public class Member {
+
+    private int memberNo;
+    private String memberName;
+    private int height;
+    private int weight;
+    private LocalDate birth;
+
+    public Member() {
+    }
+
+    public Member(int memberNo, String memberName, int height, int weight, LocalDate birth) {
+        this.memberNo = memberNo;
+        this.memberName = memberName;
+        this.height = height;
+        this.weight = weight;
+        this.birth = birth;
+    }
+
+    public int getMemberNo() {
+        return memberNo;
+    }
+
+    public void setMemberNo(int memberNo) {
+        this.memberNo = memberNo;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public LocalDate getBirth() {
+        return birth;
+    }
+
+    public void setBirth(LocalDate birth) {
+        this.birth = birth;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "memberNo=" + memberNo +
+                ", memberName='" + memberName + '\'' +
+                ", height=" + height +
+                ", weight=" + weight +
+                ", birth=" + birth +
+                '}';
+    }
+}
---
+++ chap10-api-lecture-source/src/main/java/com/ohgiraffers/api/member/run/Run.java
@@ -0,0 +1,40 @@
+package com.ohgiraffers.api.member.run;
+
+import com.ohgiraffers.api.member.Member;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Run {
+
+    public static void main(String[] args) {
+
+        String data = "1,김연아,165,47,19900905|2,양세형,167,60,19850818|3,김래원,182,80,19810319";
+
+        String[] firstSplit = data.split("\\|");
+
+        System.out.println(firstSplit.length);
+        System.out.println(Arrays.toString(firstSplit));
+
+        Member[] members = new Member[firstSplit.length];
+
+        for( int i = 0; i < firstSplit.length; i++) {
+
+            String[] secondSplit = firstSplit[i].split(",");
+
+            Member member = new Member();
+            member.setMemberName(secondSplit[1]);
+            member.setMemberNo(Integer.parseInt(firstSplit[0]));
+            member.setHeight(Integer.parseInt(firstSplit[2]));
+            member.setWeight(Integer.parseInt(firstSplit[3]));
+            LocalDate
+        }
+
+
+
+
+
+
+
+    }
+}
