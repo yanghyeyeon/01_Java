43eeee07c996ac4c871ffd07a8f4ac7904c1220a
yanghyeyeon <yhy4058@gmail.com>
2024-05-31 11:13:14 +0900

    map hashmap

--- chap12-collection-lecture-source/src/main/java/com/ohgiraffers/section03/map/run/Application1.java
+++ chap12-collection-lecture-source/src/main/java/com/ohgiraffers/section03/map/run/Application1.java
@@ -35,5 +35,32 @@ public class Application1 {
         hMap.put(12,"red apple");
         hMap.put(33,123);
 
+        System.out.println("hMap : " + hMap);
+
+        // 중복확인
+        // 같은 키값에 value를 다르게 하게되면 덮어씌워진다.
+        hMap.put(12,"yellow banana");
+
+        System.out.println("hMap : " + hMap);
+
+
+        // 값 중복 확인
+        hMap.put(12,"yellow banana");
+        hMap.put(13,"yellow banana");
+        hMap.put(14,"yellow banana");
+
+        System.out.println("hMap : " + hMap);
+
+        // remove() -> 삭제
+        // key값으로 접근해서 저장된 값을 삭제
+        hMap.remove(13);
+        hMap.remove(14);
+
+        System.out.println("hMap : " + hMap);
+
+
+        // 저장된 객체 수 확인
+        System.out.println("hMap에 저장된 객체 수 : " + hMap.size());
+
     }
 }
---
+++ chap12-collection-lecture-source/src/main/java/com/ohgiraffers/section03/map/run/Application2.java
@@ -0,0 +1,2 @@
+package com.ohgiraffers.section03.map.run;public class Application2 {
+}
