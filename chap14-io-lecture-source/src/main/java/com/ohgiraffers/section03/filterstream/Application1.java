15616eb892474c76abe8d129e807089d983cae30
yanghyeyeon <yhy4058@gmail.com>
2024-06-03 13:14:02 +0900

    io filterstream bufferedwriter & bufferedreader

--- chap14-io-lecture-source/src/main/java/com/ohgiraffers/section03/filterstream/Application1.java
+++ chap14-io-lecture-source/src/main/java/com/ohgiraffers/section03/filterstream/Application1.java
@@ -1,9 +1,6 @@
 package com.ohgiraffers.section03.filterstream;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class Application1 {
 
@@ -21,13 +18,59 @@ public class Application1 {
         BufferedWriter bw = null;
 
         try {
+
             bw = new BufferedWriter(new FileWriter("src/main/java/com/ohgiraffers/section03/filterstream/testBufferd.txt"));
+
+            bw.write("안녕하세요");
+            bw.write(10);
+            bw.write("반갑습니다");
+
+            // 버퍼를 이용하는 경우 버퍼가 가득 차지 않는 상태에서 내보낼땐
+            //flush로 강제로 내보내기를 할 수 있다.
+            bw.flush();
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
+
             if (bw != null) {
                 try {
+
+                    // close를 호출하면 내부적으로 flush()를 하고서 자원을 반납한다.
                     bw.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        // BufferedReader
+        // 버퍼에 미리 읽어온 후, 한 줄 단위로 읽어들이는 기능을 제공해준다.
+        // => 기존 스트림보다 성능이 개선됨.
+        BufferedReader br = null;
+
+        try {
+
+            br = new BufferedReader(new FileReader("src/main/java/com/ohgiraffers/section03/filterstream/testBufferd.txt"));
+
+            String temp;
+
+            /*
+            * readLine() 기능을 추가로 제공한다.
+            * 버퍼의 한 줄을 읽어와서 문자열로 반환해준다.
+            * */
+            while ((temp = br.readLine()) != null) {
+                System.out.println(temp);
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if(br != null) {
+                try {
+                    br.close();
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
