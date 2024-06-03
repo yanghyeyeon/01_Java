b2734ff00472d56e8323d4d1491a3b960254c8f4
yanghyeyeon <yhy4058@gmail.com>
2024-06-03 09:59:37 +0900

    exception

--- chap13-exception-lecture-source/src/main/java/com/ohgiraffers/section03/use/Application1.java
+++ chap13-exception-lecture-source/src/main/java/com/ohgiraffers/section03/use/Application1.java
@@ -1,2 +1,58 @@
-package com.ohgiraffers.section03.use;public class Application1 {
+package com.ohgiraffers.section03.use;
+
+import java.io.*;
+
+public class Application1 {
+
+    // io 패키지에서 예외처리
+    public static void main(String[] args) {
+
+        BufferedReader in = null;
+
+        try {
+
+            // FileReader라는 클래스에 FileNotFoundException을
+            // throw 해놓아서 try-catch로 예외처리를 해주어야함
+            in = new BufferedReader(new FileReader("test.dat"));
+
+            // 파일을 읽어서 저장할 문자열
+            String s;
+
+            //파일을 한줄씩 읽어서 출력하는 기능
+            while((s = in.readLine()) != null) {
+                System.out.println(s);
+            }
+
+        } catch (FileNotFoundException e) {
+
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+
+            // IOException
+            // In - Out 예외클래스
+            // catch 블럭은 여러개 작성할 때, 상위 타입이 하단에오고, 후손 타입이 상단에 온다.
+            throw new RuntimeException(e);
+        } finally {
+            /*
+            * finally
+            *
+            * 예외 처리 구문과 상관 없이 반드시 수행하는 경우 작성한다.
+            * 보통 사용한 자원을 반납할 목적으로 사용된다.
+            * */
+
+            try {
+                
+                // BufferReader 객체가 없을 때를 대비해서 조건문으로 작성
+                if(in != null) {
+                    
+                    // 자원 반환
+                    in.close();    
+                }
+                
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
 }
--- chap13-exception-lecture-source/src/main/java/com/ohgiraffers/section03/use/Application2.java
+++ chap13-exception-lecture-source/src/main/java/com/ohgiraffers/section03/use/Application2.java
@@ -1,2 +1,34 @@
-package com.ohgiraffers.section03.use;public class Application2 {
+package com.ohgiraffers.section03.use;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Application2 {
+
+    public static void main(String[] args) {
+
+        /*
+        * tray-with-resource
+        * JDK 1.7에서 추가된 문법
+        * close 해야 하는 인스턴스의 경우 try 옆에 괄호 안에서 생성하면
+        * 해당 try-catch블럭이 완료될때 자동으로 close 처리 해준다.
+        * */
+
+        try(BufferedReader in = new BufferedReader(new FileReader("test.dat"))) {
+
+            String s;
+
+            while((s = in.readLine()) != null) {
+                
+                System.out.println(s);
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } ;
+    }
 }
