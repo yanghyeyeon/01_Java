fa77f5d52eeb023e8a29d682df63688760d8999b
yanghyeyeon <yhy4058@gmail.com>
2024-06-04 16:51:14 +0900

    lamda 함수적 인터페이스 predicate

--- chap16-lamda-lecture-source/src/main/java/com/ohgiraffers/section02/Application5.java
+++ chap16-lamda-lecture-source/src/main/java/com/ohgiraffers/section02/Application5.java
@@ -11,9 +11,10 @@ public class Application5 {
         * Predicate : 매개변수와 boolean 리턴값이 있는 testXXX() 를 가지고 있다.
         *             이 메소드들은 매개변수 값을 이용하여 true 혹은 false 값을 리턴하는 역할을 한다.
         * */
+
         /* Predicate<T>#teat(T t) : boolean : T를 조사하여 boolean을 리턴한다. */
         Predicate<Object> predicate = value -> value instanceof String;
-        System.out.println("문자열인지 확인 : " + predicate.test("123"));
-        System.out.println("문자열인지 확인 : " + predicate.test(123));
+        System.out.println("문자열인지 확인 : " + predicate.test("123"));  //true
+        System.out.println("문자열인지 확인 : " + predicate.test(123));    //false
     }
 }
