0d669a76a6f9f28ef49c15f227367c8950490d01
yanghyeyeon <yhy4058@gmail.com>
2024-06-09 16:35:10 +0900

    stream geration

--- chap17-stream-lecture-source/src/main/java/com/ohgiraffers/section02/generation/Application2.java
+++ chap17-stream-lecture-source/src/main/java/com/ohgiraffers/section02/generation/Application2.java
@@ -33,5 +33,26 @@ public class Application2 {
         Stream<Double> doubleStream = new Random().doubles(5).boxed();
         doubleStream.forEach(value -> System.out.print(value + " "));
         System.out.println(); // 줄바꿈
+
+        // 문자열을 intStream으로 변환
+        IntStream helloworldChars = "hello world".chars(); // 문자열을 int 형으로 변경
+        helloworldChars.forEach(v -> System.out.print(v + " "));
+        System.out.println();
+
+        Stream<String> stringStream1 = Stream.of("java","oracle","jdbc");
+        Stream<String> stringStream2 = Stream.of("html", "css", "javascript");
+
+        // 스트림은 1회성으로 사용 가능하며, 이미 최종 연산이 끝난 스트림은 재사용이 불가능하다.
+        // 다시 사용할 시 IllegalStateException이 발생한다.
+//        stringStream1.forEach(System.out::println);
+//        stringStream2.forEach(System.out::println);
+
+        /*
+        * concat() : 두개의 스트림을 동일 타입 스트림으로 합치기
+        * */
+
+        System.out.println("================ concatStream ================");
+        Stream<String> concatStream = Stream.concat(stringStream1, stringStream2);
+        concatStream.forEach(System.out::println);
     }
 }
